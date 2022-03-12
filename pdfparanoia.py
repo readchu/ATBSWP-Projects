504d70520f5f27419c9deaa2548c561fc209d268
Evan Chu <49127693+readchu@users.noreply.github.com>
2022-03-12 13:26:02 -0500

    Add files via upload

---
+++ pdfparanoia.py
@@ -0,0 +1,26 @@
+"""Encrypt or decrypt every PDF in folder and subfolder from
+commandline using provided password.
+
+Will add or remove the suffix _paranoia_encrypted to each filename
+
+Will delete unencrypted files after encrypting and vice-versa
+
+Functions:
+
+
+"""
+
+#TODO: interpret command-line
+#usage
+# use argparse?
+
+#path = some WindowsPath
+# for p in path.rglob("*"):
+#    do whatever we want here, with the
+
+#TODO: make function to encrypt all folders and change name
+
+#TODO: make function to delete
+
+if __name__ = "__main__":
+
